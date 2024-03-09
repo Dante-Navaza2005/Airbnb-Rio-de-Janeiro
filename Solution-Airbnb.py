d5b9469c7b5945d842392ed42e8e5bcd29085bbe
Dante <dantenavaza2005@gmail.com>
2024-03-09 20:57:18 -0300

    nc

--- Solution-Airbnb.py
+++ Solution-Airbnb.py
@@ -44,6 +44,7 @@ dataset_folder = pathlib.Path('dataset')
 months = {'jan': 1, 'fev': 2, 'mar': 3, 'abr': 4, 'mai': 5, 'jun': 6, 'jul': 7, 'ago': 8, 'set': 9, 'out': 10, 'nov': 11, 'dez': 12}
 df_list = []
 
+
 #* concatenating all the databases into one and adding the month and year collumns
 for file in dataset_folder.iterdir() :
     month = months[file.name[:3]]
@@ -62,17 +63,19 @@ base_dataframe = pd.concat(df_list)
 """
 As there are a lot of collumns, we are going to reduce the amount of collumns to enhance the algorithm efficiency. 
 Furthermore, a rapid analysis of the data shows a significant number of redundant columns in the data for the prediction model, leading to the removal of the following collumns:
-    1)  ID: these quantitative values are unnecessary and could interfere with the final results as they would also be calculated.
+    1) Any collumn with unecessary data types that wont influence the final price such as images, verification methods, etc.
+    2) ID: these quantitative values are unnecessary and could interfere with the final results as they would also be calculated.
     2) Repeated collumns: the data contains many columns that are repeated or very similar to each other, such as the date, state, and country.
     3) Any collumn that contains hyperlinks or free-form text: besides of not containing relevant data for the desired result, it could interfere with the prediction model.
-    4) 
-    5) 
-    6) 
-    7) 
+    4) If over 50% of the data is missing it will remove that collumn
 A excel file with the first 900 rows will be generated in order to do a quick analysis of the data.    
 """
 base_dataframe.head(900).to_csv('900_rows.csv', sep=';', index=False)
 
+filtered_collumns = ['host_response_time','host_response_rate','host_is_superhost','host_listings_count','latitude','longitude','property_type','room_type','accommodates','bathrooms','bedrooms','beds','bed_type','amenities','price','security_deposit','cleaning_fee','guests_included','extra_people','minimum_nights','maximum_nights','number_of_reviews','review_scores_rating','review_scores_accuracy','review_scores_cleanliness','review_scores_checkin','review_scores_communication','review_scores_location','review_scores_value','instant_bookable','is_business_travel_ready','cancellation_policy','year','month']
+
+base_dataframe = base_dataframe.loc[:, filtered_collumns]
+print(base_dataframe)
 
 #! 5) TREAT MISSING VALUES
 
