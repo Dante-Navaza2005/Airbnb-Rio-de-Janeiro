e19787477747b96bfe507a8958d77741bce9f6c4
Dante <dantenavaza2005@gmail.com>
2024-03-23 12:54:59 -0300

    Update airbnb_deploy.py

--- airbnb_deploy.py
+++ airbnb_deploy.py
@@ -22,52 +22,54 @@ st.title("Airbnb Machine Learning Model Deployment")
 
 st.write("If you don't have the file of the model, download it [here](https://drive.google.com/file/d/1VMhrCh5l2neipciZF15Y1lBDS02lgeN5/view?usp=sharing)")
 
-final_model = st.text_input("Enter the complete path of the file of the model")
+final_model_path = st.text_input("Enter the complete directory path of the model file")
 
 #? Creating a button for each dictionary and updating the values after the user's input
 #? Some buttons such as the latitude and logitude, will have float values and others will have integer, we will adjust their values as needed.
-for item in x_numerical :
-    if item == 'latitude' or item == 'longitude' :
-        value = st.number_input(f'{item}', step=0.000001, value=0.0, format="%.6f")
-    elif item == 'extra_people' :
-        value = st.number_input(f'{item}', step=0.01, value = 0.0) #? default decimal places for floats are already two, so no format is needed
-    else :
-        value = st.number_input(f'{item}', step = 1, value = 0)
-    x_numerical[item] = value
 
-for item in x_boolean :
-    value = st.selectbox(f'{item}', ('Yes', 'No'))
-    if value == 'Yes' :
-        x_boolean[item] = 1
-    else :
-        x_boolean[item] = 0
+if final_model_path and os.path.exists(final_model_path):
+    final_model_file = os.path.join(final_model_path, "final_model.joblib")
+    for item in x_numerical :
+        if item == 'latitude' or item == 'longitude' :
+            value = st.number_input(f'{item}', step=0.000001, value=0.0, format="%.6f")
+        elif item == 'extra_people' :
+            value = st.number_input(f'{item}', step=0.01, value = 0.0) #? default decimal places for floats are already two, so no format is needed
+        else :
+            value = st.number_input(f'{item}', step = 1, value = 0)
+        x_numerical[item] = value
 
-#? We will iterate over each element of the list_value dictionary, as it is the one that stores the values the user enters
-for item in x_lists :
-    value = st.selectbox(f'{item}', x_lists[item])
-    list_values[f'{item}_{value}'] = 1
+    for item in x_boolean :
+        value = st.selectbox(f'{item}', ('Yes', 'No'))
+        if value == 'Yes' :
+            x_boolean[item] = 1
+        else :
+            x_boolean[item] = 0
 
-#? Lastly, we will create a button for the user to see the predicted value
-    
-preview_button = st.button("View the predicted value")
+    #? We will iterate over each element of the list_value dictionary, as it is the one that stores the values the user enters
+    for item in x_lists :
+        value = st.selectbox(f'{item}', x_lists[item])
+        list_values[f'{item}_{value}'] = 1
 
-if preview_button :
-    #? We will join all the dictionaries so we can transform them into a dataframe for the machine learning model to use.
-    list_values.update(x_numerical)
-    list_values.update(x_boolean)
+    #? Lastly, we will create a button for the user to see the predicted value
+        
+    preview_button = st.button("View the predicted value")
 
-    x_value_dataframe = pd.DataFrame(list_values, index = [0])
+    if preview_button :
+        #? We will join all the dictionaries so we can transform them into a dataframe for the machine learning model to use.
+        list_values.update(x_numerical)
+        list_values.update(x_boolean)
 
-    #? As the data has to be in the same order as the data the model trained upon, we will create a list containing the columns in the same order (excluding the first column which is for indexing and the last column ('Price') which was implemented after the training).
-    data = pd.read_csv("final_data.csv")
-    column_order_list = list(data.columns)[1:-1]
+        x_value_dataframe = pd.DataFrame(list_values, index = [0])
 
-    #? We will use the column order list to rearrange the columns in the dataframe
-    x_value_dataframe = x_value_dataframe[column_order_list]
+        #? As the data has to be in the same order as the data the model trained upon, we will create a list containing the columns in the same order (excluding the first column which is for indexing and the last column ('Price') which was implemented after the training).
+        data = pd.read_csv("final_data.csv")
+        column_order_list = list(data.columns)[1:-1]
 
-    st.write(final_model + r"\final_model.joblib")
-    model = joblib.load(final_model + "\final_model.joblib")
-    prediction = model.predict(x_value_dataframe)
+        #? We will use the column order list to rearrange the columns in the dataframe
+        x_value_dataframe = x_value_dataframe[column_order_list]
+
+        model = joblib.load(final_model_file)
+        prediction = model.predict(x_value_dataframe)
 
 
     st.write(f"The predicted value is R$ {prediction[0]:.2f}")
